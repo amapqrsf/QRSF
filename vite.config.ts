12ba46ef9cfbb576db0b7a50d22109090452c500
Gabriel Goldbronn <amap.qrsf@zaclys.net>
2024-10-18 12:00:39 +0200

    fix revert

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,7 @@
-
-import { svelte } from '@sveltejs/vite-plugin-svelte';
-import { defineConfig } from 'vite';
-import md from 'vite-plugin-md';
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    svelte(),
-    md()
-  ],
-});
+  plugins: [svelte()],
+})
