68808ab6244ada49aee211e0c528f43d992d97eb
Gabriel Goldbronn <amap.qrsf@zaclys.net>
2024-10-18 12:06:37 +0200

    add md

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { svelte } from '@sveltejs/vite-plugin-svelte'
-import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { defineConfig } from 'vite';
+import vitePluginMarkdown from 'vite-plugin-md'; // Ajout du plugin Markdown
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
-})
+  plugins: [
+    svelte({
+      extensions: ['.svelte', '.md'], // Permettre l'extension .md dans les fichiers Svelte
+    }),
+    vitePluginMarkdown() // Activation du plugin Markdown
+  ],
+});
