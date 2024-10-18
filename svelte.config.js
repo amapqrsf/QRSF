057da4205858a2b098e58fa4c5e796c4e81436c5
Gabriel Goldbronn <amap.qrsf@zaclys.net>
2024-10-18 11:52:19 +0200

    fix

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,26 @@
 import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
 const dev = process.env.NODE_ENV === 'development';
 
 export default {
   kit: {
     adapter: adapter({
-      pages: 'build',  // Répertoire où seront générées les pages
-      assets: 'build', // Répertoire des fichiers statiques
+      pages: 'build',
+      assets: 'build',
       fallback: 'index.html'
     }),
     paths: {
-      base: dev ? '' : '/QRSF'
+      base: dev ? '' : '/sveltekit-github-pages'
     },
     prerender: {
       default: true
     }
-  }
+  },
+  preprocess: vitePreprocess({
+    style: {
+      lang: 'scss',
+      prependData: `@import 'src/styles/global.scss';` // Si tu utilises des styles globaux
+    }
+  })
 };
