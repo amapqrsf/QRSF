929092e5a1e5c21cf6f54c914f69ff20f557cf09
Gabriel Goldbronn <amap.qrsf@zaclys.net>
2024-10-18 12:00:13 +0200

    Revert "fix"
    This reverts commit efffe861d28148f57c32f5dd789c1c3ff6c3f82c.

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import adapter from '@sveltejs/adapter-static';
-import {vitePreprocess} from "@sveltejs/vite-plugin-svelte";
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
 const dev = process.env.NODE_ENV === 'development';
 
