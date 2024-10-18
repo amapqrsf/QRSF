8b22e7c078b335150dd0fcbb589dcd8ee85c833f
Gabriel Goldbronn <amap.qrsf@zaclys.net>
2024-10-18 15:29:23 +0200

    netlify

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,7 @@
 import adapter from '@sveltejs/adapter-netlify';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+const dev = process.env.NODE_ENV === 'development';
 
 export default {
   kit: {
@@ -7,5 +10,18 @@ export default {
       edge: false,
       split: false
     }),
-  }
+    paths: {
+      base: dev ? '' : '/QRSF'  // Chemin de base pour GitHub Pages
+    },
+    prerender: {
+      default: true
+    },
+    trailingSlash: 'always'
+  },
+  preprocess: vitePreprocess({
+    style: {
+      lang: 'scss',
+      prependData: `@import 'src/styles/global.scss';`
+    }
+  })
 };
