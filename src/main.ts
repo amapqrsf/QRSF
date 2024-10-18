0d67149e6946aae62538809ec30fda2622594a9b
amapqrsf <amapqrsf@users.noreply.github.com>
2024-10-18 10:29:00 +0000

    deploy: d011b338ef436783f55311a02f4cea3afad96a4c

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import './styles/_reset.scss'
-import './styles/_global.scss'
-import App from './App.svelte'
-
-const app = new App({
-  target: document.getElementById('app')!,
-})
-
-export default app
